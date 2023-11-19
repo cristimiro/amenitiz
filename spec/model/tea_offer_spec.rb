339ae669e009e7f57c85cf1a1d279be80bdca4b2
cristimiro <cristian.miron.97@gmail.com>
2023-11-19 15:10:53 +0200

    Add tea offer spec

---
+++ spec/model/tea_offer_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../../model/tea_offer'
+
+RSpec.describe TeaOffer do
+  let(:tea_offer) { TeaOffer.new(product_price: product_price, product_count: product_count) }
+  let(:product_price) { 3.11 } 
+
+  describe '#apply_offer' do
+    subject { tea_offer.apply_offer }
+
+    context 'when product_count is 0' do
+      let(:product_count) { 0 }
+
+      it { is_expected.to eq(0) }
+    end
+
+    context 'when product_count is even' do
+      let(:product_count) { 2 }
+
+      it { is_expected.to eq(3.11) }
+    end
+
+    context 'when product_count is odd' do
+      let(:product_count) { 3 }
+
+      it { is_expected.to eq(6.22) }
+    end
+  end
+end
