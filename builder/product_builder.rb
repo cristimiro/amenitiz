e69de11b9336404948629ec16a3db69700c00151
cristimiro <cristian.miron.97@gmail.com>
2023-11-19 13:56:15 +0200

    Add product builder implementation

--- builder/product_builder.rb
+++ builder/product_builder.rb
@@ -1,6 +1,20 @@
+require_relative '../model/product'
+
 # Class used to build different instances of producrt
 class ProductBuilder
-  def self.build_product(code:)
+  PRODUCT_NAMES = {
+    'GR1' => 'Green Tea',
+    'SR1' => 'Strawberries',
+    'CF1' => 'Coffee'
+  }.freeze
 
+  PRODUCT_PRICES = {
+    'GR1' => 3.11,
+    'SR1' => 5.0,
+    'CF1' => 11.23
+  }.freeze
+
+  def self.build_product(code:)
+    Product.new(code: code, name: PRODUCT_NAMES[code], price: PRODUCT_PRICES[code])
   end
 end
