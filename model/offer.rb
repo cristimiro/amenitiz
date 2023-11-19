4d4094d531e1bdc1d64b01b9baebfda12fd1647d
cristimiro <cristian.miron.97@gmail.com>
2023-11-19 15:11:09 +0200

    Add offer initializer

--- model/offer.rb
+++ model/offer.rb
@@ -1,4 +1,9 @@
 # Offer class
 class Offer
   attr_accessor :product_price, :product_count
+
+  def initialize(product_price:, product_count:)
+    @product_price = product_price
+    @product_count = product_count
+  end
 end
