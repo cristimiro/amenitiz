484ea132e64b040d03946e59b658d011ca1e67f0
cristimiro <cristian.miron.97@gmail.com>
2023-11-19 13:35:22 +0200

    Add product class

---
+++ model/product.rb
@@ -0,0 +1,10 @@
+# Product class. Used to build different instances of Product
+class Product
+  attr_accessor :code, :name, :price
+
+  def initialize(code:, name:, price:)
+    @code = code
+    @name = name
+    @price = price
+  end
+end
