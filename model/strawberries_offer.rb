4754ea690cd7e3e1243051b3b421e087b3d03b9a
cristimiro <cristian.miron.97@gmail.com>
2023-11-19 15:24:37 +0200

    Add strawberries offer implementation

--- model/strawberries_offer.rb
+++ model/strawberries_offer.rb
@@ -2,5 +2,9 @@ require_relative 'offer'
 
 # StrawberriesOffer class
 class StrawberriesOffer < Offer
+  def apply_offer
+    return 0 if product_count.zero?
 
+    product_count < 3 ? product_count * product_price : product_count * 4.5
+  end
 end
