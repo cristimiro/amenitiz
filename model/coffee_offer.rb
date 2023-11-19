e0ce31236635985b2370730804cc31343aa2dcfc
cristimiro <cristian.miron.97@gmail.com>
2023-11-19 15:18:47 +0200

    Add coffee offer implementation

--- model/coffee_offer.rb
+++ model/coffee_offer.rb
@@ -2,5 +2,9 @@ require_relative 'offer'
 
 # CoffeeOffer class
 class CoffeeOffer < Offer
+  def apply_offer
+    return 0 if product_count.zero?
 
+    product_count < 3 ? product_count * product_price : product_count * (product_price * (2 / 3))
+  end
 end
